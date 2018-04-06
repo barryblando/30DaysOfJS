c9c56df6f51211f380f455e82487978a70885d57
Retr0_0x315 <barryblando@gmail.com>
2018-04-06 21:34:35 +0800

    Added Form Validation using RegEx.

---
+++ src/Form Validation/app.js
@@ -0,0 +1,55 @@
+// Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+  const name = document.getElementById('name');
+  const re = /^[a-zA-Z]{2,10}$/;
+
+   // if invalid add invalid class
+  if (!re.test(name.value)) {
+    name.classList.add('is-invalid');
+  } else {
+    name.classList.remove('is-invalid');
+  }
+}
+
+function validateZip() {
+  const zip = document.getElementById('zip');
+  const re = /^([0-9]){4}$/; // PH
+  // const re = /^[0-9]{5}(-[0-9]{4})?$/; // US
+
+   // if invalid add invalid class
+  if (!re.test(zip.value)) {
+    zip.classList.add('is-invalid');
+  } else {
+    zip.classList.remove('is-invalid');
+  }
+}
+
+function validateEmail() {
+  const email = document.getElementById('email');
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+   // if invalid add invalid class
+  if (!re.test(email.value)) {
+    email.classList.add('is-invalid');
+  } else {
+    email.classList.remove('is-invalid');
+  }
+}
+
+function validatePhone() {
+  const phone = document.getElementById('phone');
+  const re = /^09[0-9]{2}\s?(-?[0-9]{3})(-?[0-9]{4})$/; // PH
+  // const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/; // US
+
+   // if invalid add invalid class
+  if (!re.test(phone.value)) {
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }
+}
