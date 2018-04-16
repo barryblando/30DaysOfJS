46af1e7e3c2d910daa609ffd44b3762cb09b7de7
Retr0_0x315 <barryblando@gmail.com>
2018-04-16 13:20:20 +0800

    Fixed Async Await in Github Finder.

--- src/Github Finder/github.js
+++ src/Github Finder/github.js
@@ -10,8 +10,11 @@ class Github {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
     const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const profile = await profileResponse.json();
-    const repos = await reposResponse.json();
+    // Wait for both of them to come back
+    // if profile takes a second but repos takes 10 seconds, the whole thing is gonna take 10 seconds altogether
+    const res = await Promise.all([profileResponse, reposResponse]);
+    const dataPromises = res.map(r => r.json()); // Two extra promises which is turning it into json, await it
+    const [profile, repos] = await Promise.all(dataPromises);
 
     // return profile data
     return {
